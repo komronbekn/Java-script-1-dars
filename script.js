5b55b688c4fa068d436eeda0765ccc6f286c4437
komronbekn <komronfront@gmail.com>
2024-04-13 16:35:03 +0500

    by komronbek

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = prompt ("Ismingizni kirting")
+console.log(`Ism:${name}`);
+let surname = prompt ("Familiyangizni kiriting")
+console.log(`Familiya:${surname}`);
+let age = +prompt ("Yoshingizni kiriting")
+console.log(`Yosh:${age}`);
+let city = prompt ("Qaysi shaharda yashaysiz")
+console.log(`Shahar:${city}`);
+let subject = prompt ('Yoqtirgan faningizni kiriting')
+console.log(`Fan:${subject}`);
